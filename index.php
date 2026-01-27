a47049d2c4b0db496210df85f0674616717f4aec
Symon <fritzsymon23@gmail.com>
2026-01-27 22:54:25 +0800

    Learning 2.1: Boolean and lil bit of Improvised using If and Else

--- index.php
+++ index.php
@@ -2,11 +2,20 @@
     $name = "Symon";
     $age = 23;
     $email = "symon@gmail.com";
+    $gpa = 3.2;
+    $tax_rate = 5.1;
+    $isAdult = $age > 18;
 
     echo("Index Page <br>");
     echo("I am {$name}<br>");
     echo("My age is {$age} years old<br>");
     echo("My email is {$email}<br>");
+
+    if($isAdult){
+        echo("You are Adult");
+    } else {
+        echo("You are not");
+    }
 
     
     
