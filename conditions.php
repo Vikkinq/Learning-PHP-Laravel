91301f177851f1e03305046752c986dd6181e75b
Symon <fritzsymon23@gmail.com>
2026-01-29 22:54:25 +0800

    Learning 4: Conditional Statements (If and Else)

---
+++ conditions.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order Page | Activity #1</title>
+</head>
+<body>
+    <form action="conditions.php" method="get">
+        <div>
+            <label for="order">Name: </label>
+            <input type="text" name="name">
+        </div>
+
+        <div>
+            <label for="quantity">Age: </label>
+            <input type="number" name="age">
+        </div>
+        
+        <div>
+            <input type="submit" value="Submit">
+        </div>
+    </form>
+</body>
+</html>
+
+<?php 
+    $name = $_GET["name"];
+    $age = $_GET["age"];
+
+    echo("Name: {$name}");
+    echo("Age: {$age}");
+    
+    if($age > 18){
+        echo("You are an Adult");
+    } else {
+        echo("You are a Child");
+    }
+?>
