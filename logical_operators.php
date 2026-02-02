89f4742950822bbd43f9e72dae56746890963098
Symon <fritzsymon23@gmail.com>
2026-02-02 22:32:46 +0800

    Learning 6: Switch Case Statement

--- logical_operators.php
+++ logical_operators.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Order Page | Activity #1</title>
-</head>
-<body>
-    <form action="logical_operators.php" method="get">
-        <div>
-            <label for="order">Name: </label>
-            <input type="text" name="name">
-        </div>
-
-        <div>
-            <label for="quantity">Age: </label>
-            <input type="number" name="age">
-        </div>
-        
-        <div>
-            <input type="submit" value="Submit">
-        </div>
-    </form>
-</body>
-</html>
 
 <?php 
     $temperature = 15;
---
+++ switch_statement.php
@@ -0,0 +1,13 @@
+
+<?php 
+    $grade = "A";
+
+    switch($grade){
+        case "A":
+            echo("You are a Top Nothcer");
+        case "B":
+            echo("You are a good Student");
+        case "C":
+            echo("You are decent");
+    }
+?>
