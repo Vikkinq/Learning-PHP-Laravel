fe99f2d02460e15b74cea9dc4f118abc0863debb
Symon <fritzsymon23@gmail.com>
2026-02-05 21:42:33 +0800

    Learning 11: PHP Functions

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+    function person($name, $age){
+        echo("Good Day, Lord {$name} <br>");
+        echo("You are {$age} years old, milord <br> <br>");
+    }
+
+    function isEven($num){
+        $result = $num % 2;
+        if($result == 0){
+            echo("Even");
+        } else {
+            echo("Odd");
+        }
+    }
+
+    function calculator(float $num1, float $num2){
+        $result = $num1 * $num2;
+        echo("<br>Answer is: {$result}");
+    }
+
+    person("Symon", 24);
+    isEven(3);
+    calculator(2, 5)
+?>
