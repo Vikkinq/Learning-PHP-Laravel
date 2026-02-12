e5738a1eb08a35b65001ee76f7eedccfe0bb49ba
Symon <fritzsymon23@gmail.com>
2026-02-12 23:13:34 +0800

    Learning 13: Include Function in PHP

---
+++ Include/index.php
@@ -0,0 +1,21 @@
+<?php
+    include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Include</title>
+</head>
+<body>
+    <p>
+        Home Page
+    </p>
+</body>
+</html>
+
+<?php
+    include("footer.html")
+?>
