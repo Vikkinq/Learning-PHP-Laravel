849f9ea9fb0bc584bdb30b4ae5ba7fd6a696e94a
Symon <fritzsymon23@gmail.com>
2026-02-16 22:34:32 +0800

    Learning 15.1: Session with Forms and Inputs

--- Session/index.php
+++ Session/index.php
@@ -10,14 +10,37 @@
     <title>Document</title>
 </head>
 <body>
-    <a href="home.php">This goes to Home Page</a>
+    <form action="index.php" method="post">
+        <div>
+            <label for="username">Username: </label>
+            <input type="text" name="username" id="username" placeholder="Enter Username">
+            <label for="password">Password: </label>
+            <input type="password" name="password" placeholder="Enter Password">
+            <input type="submit">
+        </div>
+    </form>
+
+    <a href="home.php">Home Page</a>
 </body>
 </html>
 
 <?php
-    $_SESSION["username"] = "pisotoy";
-    $_SESSION["password"] = "pinisotpisot123";
+    $_SESSION["username"] = $_POST["username"];
+    $_SESSION["password"] = $_POST["password"];
+
+    /*if($_SESSION["username"] === null){
+        echo($_SESSION["username"]  . "<br>");
+    } else {
+        echo("No Username");
+    }
+
+    if(empty($_SESSION["password"])){
+        echo($_SESSION["password"] . "<br>");
+    } else {
+        echo("No Password!");
+    } */
 
     echo($_SESSION["username"]  . "<br>");
     echo($_SESSION["password"] . "<br>");
+
 ?>
