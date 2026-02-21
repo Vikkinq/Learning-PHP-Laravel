2a8fd373f30fa31c39af9d37993c504e7b95c520
Symon <fritzsymon23@gmail.com>
2026-02-21 15:38:04 +0800

    Retrieving Data from MySQL Table using WHERE clause and Non Clause and Loop to Log all Values

--- MySQL/index.php
+++ MySQL/index.php
@@ -1,6 +1,7 @@
 <?php
     include("database.php");
 
+    /* // Inserting Values to MySQL Table
     $username = "samueljackson";
     $password = "samuel123";
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
@@ -12,6 +13,35 @@
         echo("User Registered Successfully!");
     } catch(mysqli_sql_exception){
         echo("Cant register User!");
+    } */
+
+    // Retrieving Specific Value from MySQL using WHERE clause
+
+    /*
+    $sql = "SELECT * FROM users WHERE username = 'fritzsymon23'";
+    $result = mysqli_query($database_connection, $sql);
+
+    if(mysqli_num_rows($result)){
+        $row = mysqli_fetch_assoc($result);
+        echo("<br> {$row["id"]} <br>");
+        echo("<br> {$row["username"]} <br>");
+        echo("<br> {$row["registered_date"]} <br>");
+    } else {
+        echo("No User Found");
+    } */
+
+    // Finds all Value and Echo all of it without Clause
+    $sql = "SELECT * FROM users";
+    $result = mysqli_query($database_connection, $sql);
+
+    if(mysqli_num_rows($result)){
+        while($row = mysqli_fetch_assoc($result)){
+            echo("<br> {$row["id"]} <br>");
+            echo("<br> {$row["username"]} <br>");
+            echo("<br> {$row["registered_date"]} <br>");
+        }
+    } else {
+        echo("No User Found");
     }
 
     mysqli_close($database_connection);
